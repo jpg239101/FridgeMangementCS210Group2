b89c33498591697e7c8fe15d4b76639fcb902140
bdarelius246 <60321238+bdarelius246@users.noreply.github.com>
2021-04-25 15:58:16 -0400

    Did not work
    Tried to do andriod with firebase

---
+++ app/src/main/java/com/example/fridgemangementcs210group2/Register.kt
@@ -0,0 +1,15 @@
+package com.example.fridgemangementcs210group2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
