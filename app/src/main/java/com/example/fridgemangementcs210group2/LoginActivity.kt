8d0936ff88f573a9a11e73cd244023126db36bc6
bdarelius246 <60321238+bdarelius246@users.noreply.github.com>
2021-04-26 20:25:01 -0400

    Tried a different way
    Retry

--- app/src/main/java/com/example/fridgemangementcs210group2/LoginActivity.kt
+++ app/src/main/java/com/example/fridgemangementcs210group2/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.fridgemangementcs210group2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_login)
     }
 }
